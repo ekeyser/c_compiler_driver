558b325ddfade5e9bae8e399314bccd78db1d339
Emmett Keyser <emkeyser@gmail.com>
2025-06-05 10:23:14 -0500

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-type TToken = {
-    id: string
-    value: string | number
-}
+// type TToken = {
+//     id: string
+//     value: string | number
+// }
 
 enum ETokenType {
     LEFT_PAREN,
@@ -11,11 +11,17 @@ enum ETokenType {
     SEMI_COLON,
     BANG,
     BANG_EQUAL,
+    ID,
+    MAIN,
+    INT,
+    VOID,
+    NUMBER,
+    STRING_LITERAL,
 }
 
-type AstNode = {
-    id: string
-}
+// type AstNode = {
+//     id: string
+// }
 
 class Node {
 }
@@ -23,23 +29,24 @@ class Node {
 class ProgramNode extends Node {
 }
 
-const parse_expr = (tokens: Token[]) => {
-
-
-}
+// const parse_expr = (tokens: Token[]) => {
+//
+//
+// }
 
 
 const parse_program = (tokens: Token[]) => {
 
+    console.log(tokens)
     const Program = new ProgramNode()
 
     return Program
 }
 
 
-const parse_function = (tokens: Token[]) => {
-
-}
+// const parse_function = (tokens: Token[]) => {
+//
+// }
 
 
 const expect = (expected: Token, tokens: Token[]) => {
@@ -53,7 +60,7 @@ class Token {
     literal
     line
 
-    constructor(type: ETokenType, lexeme: string, literal: object, line: number) {
+    constructor(type: ETokenType, lexeme: string | number, literal: object, line: number) {
 
         this.type = type
         this.lexeme = lexeme
@@ -67,27 +74,25 @@ const tokenize = (content: string) => {
 
     const tokens: Token[] = []
     const _s = content.split("")
-    console.log(_s)
-    let position = 0
     let current = 0
     let line = 0
 
     const advance = () => {
 
-        return _s[position++]
+        return _s[current++]
     }
 
 
-    const addToken = (type: ETokenType) => {
+    const addToken = (type: ETokenType, lexeme: string | number) => {
 
-        const _token = new Token(type, "", {}, 0)
+        const _token = new Token(type, lexeme, {}, line)
         tokens.push(_token)
     }
 
 
     const isAtEnd = () => {
 
-        return false
+        return current >= _s.length
     }
 
 
@@ -109,6 +114,66 @@ const tokenize = (content: string) => {
     }
 
 
+    const _number = (_n: number) => {
+
+        const n = peek()
+        const _re = /[0-9]/g
+        if (n.match(_re)) {
+
+            current++
+            return _number(parseInt(`${_n}${n}`))
+        }
+
+        return _n
+    }
+
+
+    const _string = (_c: string) => {
+
+        const c = peek()
+        const _re = /[a-zA-Z0-9_]/g
+        if (c.match(_re)) {
+
+            current++
+            return _string(`${_c}${c}`)
+        }
+
+        return _c
+    }
+
+
+    const idOrKw = (lexeme: string) => {
+
+        switch (lexeme) {
+
+            case 'void':
+                return addToken(ETokenType.VOID, _string(lexeme))
+
+            case 'int':
+                return addToken(ETokenType.INT, _string(lexeme))
+
+            case 'main':
+                return addToken(ETokenType.MAIN, _string(lexeme))
+
+            default:
+                return addToken(ETokenType.ID, _string(lexeme))
+        }
+    }
+
+
+    const stringLiteral = (_c: string) => {
+
+        const c = peek()
+        if (c !== '"') {
+
+            current++
+            return stringLiteral(c)
+        }
+
+        return _c
+    }
+
+
     const scanToken = () => {
 
         const c = advance()
@@ -116,22 +181,25 @@ const tokenize = (content: string) => {
         switch (c) {
 
             case '(':
-                return addToken(ETokenType.LEFT_PAREN)
+                return addToken(ETokenType.LEFT_PAREN, c)
 
             case ')':
-                return addToken(ETokenType.RIGHT_PAREN)
+                return addToken(ETokenType.RIGHT_PAREN, c)
 
             case '{':
-                return addToken(ETokenType.LEFT_BRACE)
+                return addToken(ETokenType.LEFT_BRACE, c)
 
             case '}':
-                return addToken(ETokenType.RIGHT_BRACE)
+                return addToken(ETokenType.RIGHT_BRACE, c)
 
             case ';':
-                return addToken(ETokenType.SEMI_COLON)
+                return addToken(ETokenType.SEMI_COLON, c)
 
             case '!':
-                return addToken(match('=') ? ETokenType.BANG_EQUAL : ETokenType.BANG)
+                return addToken(match('=') ? ETokenType.BANG_EQUAL : ETokenType.BANG, c)
+
+            case '"':
+                return addToken(ETokenType.STRING_LITERAL, stringLiteral(c))
 
             case ' ':
             case '\t':
@@ -143,15 +211,26 @@ const tokenize = (content: string) => {
                 break
 
             default:
-                return new Error(`unrecognized token ${c}`)
+                const _reLet = /[a-zA-Z]/g
+                if (c.match(_reLet)) {
+
+                    idOrKw(_string(c))
+                    // addToken(ETokenType.BANG_EQUAL, _string(c))
+                }
+
+                const _reNum = /[0-9]/g
+                if (c.match(_reNum)) {
+
+                    addToken(ETokenType.NUMBER, _number(parseInt(c)))
+                }
+
+                return new Error(`Unrecognized character:  "${c}".`)
         }
 
     }
 
-    let _string = ""
-    for (const _c of _s) {
+    while (!isAtEnd()) {
 
-        _string += _c
         scanToken()
     }
 
@@ -165,8 +244,8 @@ int main(void)
     return 2;
 }
 `
+
 let tokens = tokenize(content)
-console.log(tokens)
 const _ast = parse_program(tokens)
 
 console.log(_ast) 
